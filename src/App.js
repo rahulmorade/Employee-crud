c68f4e0336da670d510dfe8ded0ac76d8dd17eaa
unknown <rahulmorade11@gmail.com>
2023-10-01 16:19:51 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Component/layout/Navbar";
+import About from "./Component/pages/About";
+import Contact from "./Component/pages/Contact";
+import Home from "./Component/pages/Home";
+import AddEmployee from "./Component/users/AddEmployee";
+import Edit from "./Component/users/Edit";
+import {BrowserRouter as Router,Route,Switch,Redirect}from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      
+      <Navbar />
+      <Switch>
+
+        <Route  path="/home"       component   =  {Home} />
+        <Route  path="/about"      component   =  {About} />
+        <Route  path="/contact"    component   =  {Contact} />
+        <Route exact path="/"           component   =  {AddEmployee} />
+        <Route  path="/edit/:id"        component   =  {Edit} />
+        
+      </Switch>
+      
+    </div>
+    </Router>
+    
+  );
+}
+
+export default App;
